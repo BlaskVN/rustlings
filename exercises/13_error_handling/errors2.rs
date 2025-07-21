882e6e540a98961789edcd70a89aaf6855d8ae43
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2025-07-21 09:16:11 +0700

    complete: 13_error_handling

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -4,13 +4,13 @@
 // the `total_cost` function will calculate the total cost of the items. Since
 // the player typed in the quantity, we get it as a string. They might have
 // typed anything, not just numbers!
-//
+
 // Right now, this function isn't handling the error case at all. What we want
 // to do is: If we call the `total_cost` function on a string that is not a
 // number, that function will return a `ParseIntError`. In that case, we want to
 // immediately return that error from our function and not try to multiply and
 // add.
-//
+
 // There are at least two ways to implement this that are both correct. But one
 // is a lot shorter!
 
@@ -22,7 +22,8 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
 
     // TODO: Handle the error case as described above.
     let qty = item_quantity.parse::<i32>();
-
+    let qty = qty?;
+    
     Ok(qty * cost_per_item + processing_fee)
 }
 
